5861afe098c43b656661b18562c4e628ad22c14f
cristianolimapsd-stack <cristianolima.psd@gmail.com>
2026-04-22 07:32:46 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Space_Mono } from 'next/font/google'
-import './globals.css'
+import './global.css'
 
 const spaceMono = Space_Mono({
   weight: ['400', '700'],
